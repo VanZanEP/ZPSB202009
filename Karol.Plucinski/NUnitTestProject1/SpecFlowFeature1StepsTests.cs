3344648bdb94b07cea3944662a6eff37573c3e0d
karolplu123 <58879229+karolplu123@users.noreply.github.com>
2021-02-13 23:55:19 +0100

    Add files via upload

---
+++ Karol.Plucinski/NUnitTestProject1/SpecFlowFeature1StepsTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpecFlowProject1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpecFlowProject1.Tests
+{
+    [TestClass()]
+    public class SpecFlowFeature1StepsTests
+    {
+        [TestMethod()]
+        public void SpecFlowFeature1StepsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
